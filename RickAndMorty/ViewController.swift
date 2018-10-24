65358967fc9e394f4f65f628c14133510ccd9a4a
Xavier <xwatts2323@Xaviers-MacBook-Pro.local>
2018-10-24 13:24:36 -0600

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Xavier on 10/24/18.
+//  Copyright © 2018 Xavier ios dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
